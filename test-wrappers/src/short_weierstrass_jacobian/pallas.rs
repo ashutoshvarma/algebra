26ebbd08cc30b12e11d3d90260c7170e0669b73c
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-24 20:57:37 +0530

    ad tests for wrapper types

---
+++ test-wrappers/src/short_weierstrass_jacobian/pallas.rs
@@ -0,0 +1,34 @@
+use ark_ec::{
+    models::wrapped::short_weierstrass_jacobian::{GroupAffine, GroupProjective},
+    AffineCurve,
+};
+use ark_pallas::{Affine, Projective};
+use ark_std::{rand::Rng, test_rng};
+
+use ark_algebra_test_templates::{
+    curves::{curve_tests, sw_tests},
+    groups::group_test,
+};
+type PallasAffine = GroupAffine<Affine>;
+type PallasProjective = GroupProjective<Projective>;
+
+#[test]
+fn test_projective_curve() {
+    curve_tests::<PallasProjective>();
+    // sw_tests::<PallasParameters>();
+}
+
+#[test]
+fn test_projective_group() {
+    let mut rng = test_rng();
+    let a: PallasProjective = rng.gen();
+    let b: PallasProjective = rng.gen();
+    group_test(a, b);
+}
+
+#[test]
+fn test_generator() {
+    let generator = PallasAffine::prime_subgroup_generator();
+    assert!(generator.is_on_curve());
+    assert!(generator.is_in_correct_subgroup_assuming_on_curve());
+}
