9ec48358c6a4af743217461244775daef7216cf6
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-25 16:31:28 +0530

    impl Group trait for wrapped edward curves

--- ec/src/models/wrapped/twisted_edwards_extended.rs
+++ ec/src/models/wrapped/twisted_edwards_extended.rs
@@ -7,12 +7,14 @@ use crate::models::TEModelParameters;
 use crate::wrapped::Wrapped;
 pub use crate::wrapped::{GroupAffine, GroupProjective};
 // use ark_ff::PrimeField;
-use ark_std::ops::{AddAssign, MulAssign, Sub, SubAssign};
+use ark_std::ops::{Add, AddAssign, MulAssign, Sub, SubAssign};
 
 use ark_std::rand::{
     distributions::{Distribution, Standard},
     Rng,
 };
+
+use ark_ff::Zero;
 
 // type _GroupAffine<P> = GroupAffine<GroupAffineED<P>>;
 
@@ -48,6 +50,13 @@ impl<P: TEModelParameters> GroupAffine<GroupAffineED<P>> {
     }
 }
 
+impl<'a, P: TEModelParameters> Add<&'a Self> for GroupAffine<GroupAffineED<P>> {
+    type Output = Self;
+    fn add(self, other: &'a Self) -> Self {
+        GroupAffine(GroupAffineED::<P>::add(*self.wrapped(), other.wrapped()))
+    }
+}
+
 impl<'a, P: TEModelParameters> AddAssign<&'a Self> for GroupAffine<GroupAffineED<P>> {
     fn add_assign(&mut self, other: &'a Self) {
         GroupAffineED::<P>::add_assign(self.mut_wrapped(), other.wrapped())
@@ -80,8 +89,6 @@ impl<P: TEModelParameters> Distribution<GroupAffine<GroupAffineED<P>>> for Stand
     }
 }
 
-// ark_ff::impl_additive_ops_from_ref!(_GroupAffine, TEModelParameters);
-
 impl<P: TEModelParameters> core::str::FromStr for GroupAffine<GroupAffineED<P>>
 where
     P::BaseField: core::str::FromStr<Err = ()>,
@@ -92,6 +99,116 @@ where
         match GroupAffineED::<P>::from_str(s) {
             Ok(v) => Ok(GroupAffine(v)),
             Err(e) => Err(e),
+        }
+    }
+}
+
+// // ark_ff::impl_additive_ops_from_ref!(_GroupAffine, TEModelParameters);
+// `Add` in above macro conflicts as its already been impl for generic GroupAffine
+// So we have to manually impl rest of traits here
+
+#[allow(unused_qualifications)]
+impl<'a, P: TEModelParameters> core::ops::Add<&'a mut Self> for GroupAffine<GroupAffineED<P>> {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, other: &'a mut Self) -> Self {
+        let mut result = self;
+        result.add_assign(&*other);
+        result
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<P: TEModelParameters> core::ops::Sub<Self> for GroupAffine<GroupAffineED<P>> {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, other: Self) -> Self {
+        let mut result = self;
+        result.sub_assign(&other);
+        result
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a, P: TEModelParameters> core::ops::Sub<&'a mut Self> for GroupAffine<GroupAffineED<P>> {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, other: &'a mut Self) -> Self {
+        let mut result = self;
+        result.sub_assign(&*other);
+        result
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<P: TEModelParameters> core::iter::Sum<Self> for GroupAffine<GroupAffineED<P>> {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::zero(), core::ops::Add::add)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a, P: TEModelParameters> core::iter::Sum<&'a Self> for GroupAffine<GroupAffineED<P>> {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(Self::zero(), core::ops::Add::add)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<P: TEModelParameters> core::ops::AddAssign<Self> for GroupAffine<GroupAffineED<P>> {
+    fn add_assign(&mut self, other: Self) {
+        self.add_assign(&other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<P: TEModelParameters> core::ops::SubAssign<Self> for GroupAffine<GroupAffineED<P>> {
+    fn sub_assign(&mut self, other: Self) {
+        self.sub_assign(&other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a, P: TEModelParameters> core::ops::AddAssign<&'a mut Self>
+    for GroupAffine<GroupAffineED<P>>
+{
+    fn add_assign(&mut self, other: &'a mut Self) {
+        self.add_assign(&*other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a, P: TEModelParameters> core::ops::SubAssign<&'a mut Self>
+    for GroupAffine<GroupAffineED<P>>
+{
+    fn sub_assign(&mut self, other: &'a mut Self) {
+        self.sub_assign(&*other)
+    }
+}
+
+mod group_impl {
+    use super::*;
+    use crate::group::Group;
+
+    impl<P: TEModelParameters> Group for GroupAffine<GroupAffineED<P>> {
+        type ScalarField = P::ScalarField;
+
+        #[inline]
+        fn double(&self) -> Self {
+            GroupAffine(self.wrapped().double())
+        }
+
+        #[inline]
+        fn double_in_place(&mut self) -> &mut Self {
+            // let mut tmp = *self;
+            // tmp += &*self;
+            // *self = tmp;
+            // self
+            self.mut_wrapped().double_in_place();
+            self
         }
     }
 }
