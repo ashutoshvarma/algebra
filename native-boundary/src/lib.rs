6b24e6ed0cc10436631d9d60591e6cff4700d8a7
Ashutosh Varma <ashutoshvarma11@live.com>
2021-07-05 19:56:20 +0530

    feat: move ark-ec inside native boundary

--- native-boundary/src/lib.rs
+++ native-boundary/src/lib.rs
@@ -1,7 +1,19 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
+#![warn(unused, future_incompatible, nonstandard_style, rust_2018_idioms)]
+#![forbid(unsafe_code)]
+#![allow(
+    clippy::op_ref,
+    clippy::suspicious_op_assign_impl,
+    clippy::many_single_char_names
+)]
 
 #[macro_use]
-extern crate alloc;
+extern crate derivative;
+
+#[macro_use]
+extern crate ark_std;
+
+mod ark_ec;
 
 pub mod boundary;
 pub mod curves;
